4815bde6801ba1e7937143229d70abeb7b642820
Administrator <admin@example.com>
2021-04-14 21:44:13 +0800

    集群一键部署脚本

--- 集群搭建脚本/clear.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash 
-systemctl stop mysql@bootstrap.service
-ps -ef | grep mysqld
-rm -rf /home/mysqldata
-rm -rf /data
-rm -rf /var/log/mysqld.log
--- 集群搭建脚本/emqx_stop.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash 
-ps -ef | grep emqx | grep -v grep 
-if [ $? -eq 0 ];then 
-   /usr/local/src/emqx-rel/_rel/emqx/bin/emqx stop 
-   ps -ef | grep emqx | grep -v grep | awk -F' ' '{print $2}' | xargs kill -9
-fi 
- 
-ps -ef | grep emqx 
-
